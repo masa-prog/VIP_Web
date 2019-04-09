24a0735990163db6c8c389326cf347dead2ce87c
masa-prog <masaki.prog@gmail.com>
2019-04-09 11:25:47 +0900

    miyajima

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+// スムーススクロールの実装
+
+$(function(){
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+
+      console.log(position);
+
+      // スムーススクロール(navの分引く)
+      $('body,html').animate({scrollTop:position-64}, speed, 'swing');
+
+      return false;
+   });
+});
