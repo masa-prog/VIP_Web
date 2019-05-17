eb120d65cfebb17f9ef3d47931b1e27f125c9ab7
masa-prog <masaki.prog@gmail.com>
2019-05-17 15:22:42 +0900

    side navを追加

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+ // document.addEventListener('DOMContentLoaded', function() {
+ //    var elems = document.querySelectorAll('.slider');
+ //    var instances = M.Slider.init(elems, options);
+ //  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+  	$('.sidenav').sidenav({
+      edge:'right',
+    }
+    );
+  });
